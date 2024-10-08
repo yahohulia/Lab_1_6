2b070d651b809d6bc14a499dcc3339715ca1d9a4
yahohulia <yaroslavhohulia@gmail.com>
2024-10-08 16:28:38 +0300

    Update Source2.cpp

--- Lab_1_6/Lab_1_6_2/Source2.cpp
+++ Lab_1_6/Lab_1_6_2/Source2.cpp
@@ -2,37 +2,25 @@
 
 int main()
 {
-	//Îïèñó çì³ííèõ òèï³â int, float, unsigned short.
 	int nA;
 	float fltB;
 	unsigned short wC;
 
-	//²í³ö³þâàííÿ çì³ííèõ, ùî îïèñàíí³ â ï.1 äàíîãî çàâäàííÿ
 	nA = 13915;
 	fltB = 4.78e5;
 	wC = 9765;
-
-	//Îïèñó çì³ííèõ òèï³â double, int, char.
 
 	double dblD;
 	int nE;
 	char chF;
 
-	//²í³ö³þâàííÿ çì³ííèõ
-
-	//çà äîïîìîãîþ íåÿâíîãî ïðèâåäåííÿ òèï³â
-
 	dblD = nA;
 	nE = fltB;
 	chF = wC;
 
-	//çà äîïîìîãîþ ÿâíîãî ïðèâåäåííÿ
-
 	dblD = (double)nA;
 	nE = (int)fltB;
 	chF = (char)wC;
-
-	//ç îáõîäîì ñóâîðî¿ òèï³çàö³¿
 
 	double* pdblD;
 	void* pV;
